88b7b733888b812f27a7b361abefb121045e20df
bli farda <134302929+farda222@users.noreply.github.com>
2023-08-19 09:33:51 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    fontFamily: {
+      'montserrat': ['Montserrat', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
